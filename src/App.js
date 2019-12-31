684c611690b654488b6815839a86dac1d892428b
RoyChen <roy850323@gmail.com>
2019-12-31 23:27:49 +0800

    react todo app

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
+import axios from 'axios';
+
+
+class App extends Component {
+	state = {
+		todos: []
+	};
+
+	componentDidMount() {
+		axios
+			.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+			.then((res) => this.setState({ todos: res.data }));
+	}
+
+	// Toggle Complete
+	markComplete = (id) => {
+		this.setState({
+			todos: this.state.todos.map((todo) => {
+				if (todo.id === id) {
+					todo.completed = !todo.completed;
+				}
+				return todo;
+			})
+		});
+	};
+
+	// Delete Todo
+	delTodo = (id) => {
+		axios
+			.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+			.then((res) =>
+				this.setState({
+					todos: [...this.state.todos.filter((todo) => todo.id !== id)]
+				})
+			);
+	};
+
+	// Add Todo
+	addTodo = (title) => {
+		axios
+			.post('https://jsonplaceholder.typicode.com/todos', {
+				title,
+				completed: false
+			})
+			.then((res) => {
+				res.data.id = uuid.v4();
+				this.setState({ todos: [...this.state.todos, res.data] });
+			});
+	};
+
+	render() {
+		return (
+				<div className='App'>
+					<div className='container'>
+						<Header />
+						<React.Fragment>
+							<AddTodo addTodo={this.addTodo} />
+							<Todos
+								todos={this.state.todos}
+								markComplete={this.markComplete}
+								delTodo={this.delTodo}
+							/>
+						</React.Fragment>
+					</div>
+				</div>
+		);
+	}
+}
+
+export default App;
