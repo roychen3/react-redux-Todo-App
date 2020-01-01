8fdf6b49a68952dfa1f84e700c1ef397578612d1
RoyChen <roy850323@gmail.com>
2020-01-01 19:49:49 +0800

    update css

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -22,9 +22,9 @@ export class TodoItem extends Component {
   render() {
     const { id, title, completed } = this.props.todo;
     return (
-      <div>
+      <div className="todo-item">
+        <input type="checkbox" onChange={this.markComplete.bind(this, id)} checked={completed} />
         <p style={this.getStyle()}>
-          <input type="checkbox" onChange={this.markComplete.bind(this, id)} checked={completed} /> {' '}
           {title}
         </p>
         <button onClick={this.delTodo.bind(this, id)} >x</button>
